f71a84239da0702b8bf5058219d17237f5005558
jara-git <59502170+jara-git@users.noreply.github.com>
2020-03-29 11:36:30 +0200

    Update counter.js

--- src/components/counter.js
+++ src/components/counter.js
@@ -12,7 +12,7 @@ export default function Counter() {
       <button className='sum' onClick={() => setCount(count + 1)}>
         +1
       </button>
-      <input className='input_num' placeholder='Set initial value' type='number' onChange={(event) => setCount (event.target.value)}></input>
+      <input className='input_num' placeholder='Set initial value' type='number' onChange={(event)=>setCount(Number(event.target.value))}></input>
       <button className='rest' onClick={() => setCount(count - 1)}>
         -1
       </button>
